6b952bc6c9900a78e4a0a76d4782d542fdb01641
Valentin Vuillon <valentin.vuillon@gmail.com>
2025-05-30 11:20:21 +0200

    added the first version of the website

---
+++ Milestones/milestone2/first_website/js/main.js
@@ -0,0 +1,6 @@
+// Main JavaScript for overall page setup
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Page Loaded. All visualizations will be initialized.");
+    
+    // To do: add logic here for setting up the page, like interactive buttons, loading data, etc.
+});
