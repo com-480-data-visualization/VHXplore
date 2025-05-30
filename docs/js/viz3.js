bb62255c6e38c1949c56d1a7e93ada1081441f64
Valentin Vuillon <valentin.vuillon@gmail.com>
2025-05-30 16:28:14 +0200

    final changes

--- docs/js/viz3.js
+++ docs/js/viz3.js
@@ -1,6 +1,6 @@
 // script.js
 const margin = { top: 20, right: 20, bottom: 50, left: 100 };
-const width = 420 - margin.left - margin.right;
+const width = 500 - margin.left - margin.right;
 const height = 450 - margin.top - margin.bottom;
 
 d3.csv("data/alldata_processed.csv", d3.autoType).then(data => {
